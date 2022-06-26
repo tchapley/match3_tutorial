848e092a418ca8bcaf2f695ab45bb91922fb49bf
TJ Chapley <tj@chapley.com>
2022-06-26 11:06:16 -0400

    Start of mouse input parsing

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -7,6 +7,8 @@ export(int) var y_start
 export(int) var offset
 
 var all_pieces := []
+var first_touch := Vector2.ZERO
+var final_touch := Vector2.ZERO
 var possible_pieces := [
 	preload("res://scenes/blue_piece.tscn"),
 	preload("res://scenes/green_piece.tscn"),
@@ -23,6 +25,10 @@ func _ready() -> void:
 	spawn_pieces()
 
 
+func _process(_delta: float) -> void:
+	touch_input()
+
+
 func make_2d_array() -> Array:
 	var array := []
 	for i in width:
@@ -35,6 +41,12 @@ func make_2d_array() -> Array:
 func grid_to_pixel(column: int, row: int) -> Vector2:
 	var new_x: int = x_start + offset * column
 	var new_y: int = y_start + -offset * row
+	return Vector2(new_x, new_y)
+
+
+func pixel_to_grid(x: float, y: float) -> Vector2:
+	var new_x = round((x - x_start) / offset)
+	var new_y = round((y - y_start) / -offset)
 	return Vector2(new_x, new_y)
 
 
@@ -63,3 +75,12 @@ func match_at(col: int, row: int, color: String) -> bool:
 				if all_pieces[col][row - 1].color == color and all_pieces[col][row - 2].color == color:
 					return true
 	return false
+
+
+func touch_input() -> void:
+	if Input.is_action_just_pressed("ui_touch"):
+		first_touch = get_global_mouse_position()
+		var grid_position: Vector2 = pixel_to_grid(first_touch.x, first_touch.y)
+		print(grid_position)
+	if Input.is_action_just_released("ui_touch"):
+		final_touch = get_global_mouse_position()
